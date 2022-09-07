6819350c9c983b2dd290b3202000ab94ca2c763f
alain sardin <alain.sardin001@gmail.com>
2022-09-07 13:50:50 +0200

    ajout du script creation de ressource du brief10

---
+++ brief10_creationR.sh
@@ -0,0 +1,45 @@
+## Brief10 : Docker + Azure = love
+## Script Done by : Yvette , Driton, Alain
+
+###########################################################
+## Script For Web APP Wordpress - autoscaled - + MariaDB ##
+###########################################################
+
+#variable preset
+group=Groupe5_Brief10_YDA
+location=westeurope
+appserviceplanname=g5b10ydaasp
+webappname=Groupe5_Brief10_YDA
+username=Admin1
+password=Adminpass1
+
+###########################################################
+##Resource group
+createRG(){
+    echo " creating ressource groupe "
+    az group create -n $group -l $location
+}
+
+############################################################
+## create webapp adqsdq dqdq 
+createApp(){
+    echo " creating app plan "
+    az appservice plan create -g $group  -n $appserviceplanname --is-linux --number-of-workers 4 --sku P1V2
+    echo " creating Web app "
+    az webapp create --resource-group $group --plan $appserviceplanname --name $webappname --deployment-container-image-name alaincloud/mediawiki:stable
+    echo " creating slots"
+    # reste à faire
+
+    #echo " modif WP-config "
+    #az webapp config appsettings set -n alainb8wap -g $group --settings MARIA_DB_HOST="alainb8-mdb.mariadb.database.azure.com" MARIA_DB_USER="$username"  MARIA_DB_PASSWORD="$password"  WEBSITES_ENABLE_APP_SERVICE_STORAGE=TRUE
+}
+
+############################################################
+#tout creer
+createAll(){
+    createRG
+    createApp
+}
+
+createAll
+echo "installation terminée"
